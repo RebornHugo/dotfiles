244639bd2963e76eef52b98fd03ac0796020de01
RebornHugo <kiss152535@gmail.com>
2022-11-17 22:55:45 +0800

    feat(dap): refactor dap

--- mackup/.config/nvim/lua/user/dap.lua
+++ mackup/.config/nvim/lua/user/dap.lua
@@ -17,98 +17,160 @@ dap_install.setup({})
 
 dap_install.config("python", {})
 -- add other configs here
--- TODO: setting dap
--- require('dap-python').setup("/usr/local/bin/python3")
 
 -- Setup dap for python
-local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
+
+--          adapters
+-- ──────────────────────────────
+dap.adapters.python_launch = {
+	type = "executable",
+	command = vim.fn.expand("python3"),
+	args = { "-m", "debugpy.adapter" },
+	initialize_timeout_sec = 5,
+}
+dap.adapters.python_attach = {
+	type = "server",
+	host = "127.0.0.1",
+	port = "5678",
+}
+
+--          configs
+-- ──────────────────────────────
+dap.configurations.python = {
+	{
+		name = "Launch script",
+		type = "python_launch",
+		request = "launch",
+		program = "${file}",
+		args = function()
+			local args = vim.fn.input({ prompt = "Script args: " })
+			args = vim.fn.split(args, " ")
+			return args
+		end,
+		cwd = "${workspaceFolder}",
+		pythonPath = "python3",
+		-- subProcess=true,
+	},
+	{
+		name = "Launch module",
+		type = "python_launch",
+		request = "launch",
+		cwd = "${workspaceFolder}",
+		module = function()
+			local name = vim.fn.expand("%:r")
+			name = string.gsub(name, "/", ".")
+			name = string.gsub(name, "\\", ".")
+			return name
+		end,
+		args = function()
+			local args = vim.fn.input({ prompt = "Module args: " })
+			args = vim.fn.split(args, " ")
+			return args
+		end,
+		pythonPath = "python3",
+	},
+	{
+		name = "Attach",
+		type = "python_attach",
+		request = "attach",
+	},
+}
+
 pcall(function()
-  -- mason installed debugpy is in a brand python venv, which doesn't have dependency we need, e.g. numpy
-  -- require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
-  require("dap-python").setup("python")
+	-- mason installed debugpy is in a brand python venv, which doesn't have dependency we need, e.g. numpy
+	-- require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
+	require("dap-python").setup("python")
 end)
 
 dapui.setup({
-  icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
-  mappings = {
-    -- Use a table to apply multiple mappings
-    expand = { "<CR>", "<2-LeftMouse>" },
-    open = "o",
-    remove = "d",
-    edit = "e",
-    repl = "r",
-    toggle = "t",
-  },
-  -- Use this to override mappings for specific elements
-  element_mappings = {
-    -- Example:
-    -- stacks = {
-    --   open = "<CR>",
-    --   expand = "o",
-    -- }
-  },
-  -- Expand lines larger than the window
-  -- Requires >= 0.7
-  expand_lines = true,
-  -- Layouts define sections of the screen to place windows.
-  -- The position can be "left", "right", "top" or "bottom".
-  -- The size specifies the height/width depending on position. It can be an Int
-  -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
-  -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
-  -- Elements are the elements shown in the layout (in order).
-  -- Layouts are opened in order so that earlier layouts take priority in window sizing.
-  layouts = {
-    {
-      elements = {
-      -- Elements can be strings or table with id and size keys.
-        { id = "scopes", size = 0.25 },
-        "breakpoints",
-        "stacks",
-        "watches",
-      },
-      size = 40, -- 40 columns
-      position = "left",
-    },
-    {
-      elements = {
-        "repl",
-        "console",
-      },
-      size = 0.25, -- 25% of total lines
-      position = "bottom",
-    },
-  },
-  controls = {
-    -- Requires Neovim nightly (or 0.8 when released)
-    enabled = true,
-    -- Display controls in this element
-    element = "repl",
-    icons = {
-      pause = "",
-      play = "",
-      step_into = "",
-      step_over = "",
-      step_out = "",
-      step_back = "",
-      run_last = "↻",
-      terminate = "□",
-    },
-  },
-  floating = {
-    max_height = nil, -- These can be integers or a float between 0 and 1.
-    max_width = nil, -- Floats will be treated as percentage of your screen.
-    border = "single", -- Border style. Can be "single", "double" or "rounded"
-    mappings = {
-      close = { "q", "<Esc>" },
-    },
-  },
-  windows = { indent = 1 },
-  render = {
-    max_type_length = nil, -- Can be integer or nil.
-    max_value_lines = 100, -- Can be integer or nil.
-  }
+	icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
+	mappings = {
+		-- Use a table to apply multiple mappings
+		expand = { "<CR>", "<2-LeftMouse>" },
+		open = "o",
+		remove = "d",
+		edit = "e",
+		repl = "r",
+		toggle = "t",
+	},
+	-- Use this to override mappings for specific elements
+	element_mappings = {
+		-- Example:
+		-- stacks = {
+		--   open = "<CR>",
+		--   expand = "o",
+		-- }
+	},
+	-- Expand lines larger than the window
+	-- Requires >= 0.7
+	expand_lines = true,
+	-- Layouts define sections of the screen to place windows.
+	-- The position can be "left", "right", "top" or "bottom".
+	-- The size specifies the height/width depending on position. It can be an Int
+	-- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
+	-- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
+	-- Elements are the elements shown in the layout (in order).
+	-- Layouts are opened in order so that earlier layouts take priority in window sizing.
+	layouts = {
+		{
+			elements = {
+				-- Elements can be strings or table with id and size keys.
+				{ id = "scopes", size = 0.25 },
+				"breakpoints",
+				"stacks",
+				"watches",
+			},
+			size = 40, -- 40 columns
+			position = "left",
+		},
+		{
+			elements = {
+				"repl",
+				"console",
+			},
+			size = 0.25, -- 25% of total lines
+			position = "bottom",
+		},
+	},
+	controls = {
+		-- Requires Neovim nightly (or 0.8 when released)
+		enabled = true,
+		-- Display controls in this element
+		element = "repl",
+		icons = {
+			pause = "",
+			play = "",
+			step_into = "",
+			step_over = "",
+			step_out = "",
+			step_back = "",
+			run_last = "↻",
+			terminate = "□",
+		},
+	},
+	floating = {
+		max_height = nil, -- These can be integers or a float between 0 and 1.
+		max_width = nil, -- Floats will be treated as percentage of your screen.
+		border = "single", -- Border style. Can be "single", "double" or "rounded"
+		mappings = {
+			close = { "q", "<Esc>" },
+		},
+	},
+	windows = { indent = 1 },
+	render = {
+		max_type_length = nil, -- Can be integer or nil.
+		max_value_lines = 100, -- Can be integer or nil.
+	},
 })
 
+-- TODO: check launch.json
+require("dap.ext.vscode").load_launchjs(vim.fn.getcwd() .. "/.vscode/launch.json", vim.fn.getcwd() .. "/launch.json", {
+	python_launch = { "python" },
+	python_attach = { "python" },
+})
+
+-- TODO: set debugger line fg color
 vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
 -- vim.fn.sign_define('DapBreakpoint', {text='🟥', texthl='', linehl='', numhl=''})
 -- vim.fn.sign_define('DapStopped', {text='⭐️', texthl='', linehl='', numhl=''})
@@ -124,10 +186,12 @@ vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignErro
 -- vim.fn.sign_define('DapLogPoint', { text='', texthl='DapLogPoint', linehl='DapLogPoint', numhl= 'DapLogPoint' })
 -- vim.fn.sign_define('DapStopped', { text='', texthl='DapStopped', linehl='DapStopped', numhl= 'DapStopped' })
 
- 
 -- vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = ""})
-vim.fn.sign_define("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = ""})
-vim.fn.sign_define("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = ""})
+vim.fn.sign_define(
+	"DapBreakpointCondition",
+	{ text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" }
+)
+vim.fn.sign_define("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
 	dapui.open()
@@ -147,20 +211,20 @@ if not dap_virtual_text_ok then
 	return
 end
 
-dap_virtual_text.setup {
-    enabled = true,                        -- enable this plugin (the default)
-    enabled_commands = true,               -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
-    highlight_changed_variables = true,    -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-    highlight_new_as_changed = false,      -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-    show_stop_reason = true,               -- show stop reason when stopped for exceptions
-    commented = false,                     -- prefix virtual text with comment string
-    only_first_definition = true,          -- only show virtual text at first definition (if there are multiple)
-    all_references = false,                -- show virtual text on all all references of the variable (not only definitions)
-    filter_references_pattern = '<module', -- filter references (not definitions) pattern when all_references is activated (Lua gmatch pattern, default filters out Python modules)
-    -- experimental features:
-    virt_text_pos = 'eol',                 -- position of virtual text, see `:h nvim_buf_set_extmark()`
-    all_frames = false,                    -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-    virt_lines = false,                    -- show virtual lines instead of virtual text (will flicker!)
-    virt_text_win_col = nil                -- position the virtual text at a fixed window column (starting from the first text column) ,
-                                           -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
-}
+dap_virtual_text.setup({
+	enabled = true, -- enable this plugin (the default)
+	enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+	highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+	highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+	show_stop_reason = true, -- show stop reason when stopped for exceptions
+	commented = false, -- prefix virtual text with comment string
+	only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
+	all_references = false, -- show virtual text on all all references of the variable (not only definitions)
+	filter_references_pattern = "<module", -- filter references (not definitions) pattern when all_references is activated (Lua gmatch pattern, default filters out Python modules)
+	-- experimental features:
+	virt_text_pos = "eol", -- position of virtual text, see `:h nvim_buf_set_extmark()`
+	all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+	virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
+	virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
+	-- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
+})
