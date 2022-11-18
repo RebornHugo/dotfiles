9cdcf753fb801ab7abfe064a1f84203d3f621e0e
RebornHugo <kiss152535@gmail.com>
2022-11-19 02:35:06 +0800

    feat(nvim): refactor bufferline and disable lsp_virtual_lines

--- mackup/.config/nvim/lua/user/bufferline.lua
+++ mackup/.config/nvim/lua/user/bufferline.lua
@@ -1,127 +1,126 @@
 local status_ok, bufferline = pcall(require, "bufferline")
 if not status_ok then
-  return
+	return
 end
 
-local mocha = require("catppuccin.palettes").get_palette "mocha"
-bufferline.setup {
-  options = {
-    close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-    right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-    offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
-    separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
-  },
-  highlights = require("catppuccin.groups.integrations.bufferline").get {
-    styles = { "italic", "bold" },
-    custom = {
-      all = {
-        fill = { bg = "#000000" },
-      },
-      mocha = {
-        background = { fg = mocha.text },
-      },
-      latte = {
-        background = { fg = "#000000" },
-      },
-    },
-  },
+local setup = {
+	options = {
+		close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+		right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+		offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+		separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
+	},
 }
 
--- bufferline.setup {
---   options = {
---     close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
---     right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
---     offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
---     separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
---   },
+local highlights = {}
+local cat_status_ok, cat = pcall(require, "catppuccin.groups.integrations.bufferline")
+if cat_status_ok then
+	local mocha = require("catppuccin.palettes").get_palette("mocha")
+	highlights = cat.get({
+		styles = { "italic", "bold" },
+		custom = {
+			-- all = {
+			-- 	fill = { bg = "#000000" },
+			-- },
+			mocha = {
+				background = { fg = mocha.overlay2 },
+			},
+			latte = {
+				background = { fg = "#000000" },
+			},
+		},
+	})
+else
+	highlights = {
+		fill = {
+			fg = { attribute = "fg", highlight = "#ff0000" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
 
---   highlights = {
---     fill = {
---       fg = { attribute = "fg", highlight = "#ff0000" },
---       bg = { attribute = "bg", highlight = "TabLine" },
---     },
+		background = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
 
---     background = {
---       fg = { attribute = "fg", highlight = "TabLine" },
---       bg = { attribute = "bg", highlight = "TabLine" },
---     },
+		buffer_visible = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
 
---     buffer_visible = {
---       fg = { attribute = "fg", highlight = "TabLine" },
---       bg = { attribute = "bg", highlight = "TabLine" },
---     },
+		close_button = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
+		close_button_visible = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
 
---     close_button = {
---       fg = { attribute = "fg", highlight = "TabLine" },
---       bg = { attribute = "bg", highlight = "TabLine" },
---     },
---     close_button_visible = {
---       fg = { attribute = "fg", highlight = "TabLine" },
---       bg = { attribute = "bg", highlight = "TabLine" },
---     },
---    
---     tab_selected = {
---       fg = { attribute = "fg", highlight = "Normal" },
---       bg = { attribute = "bg", highlight = "Normal" },
---     },
+		tab_selected = {
+			fg = { attribute = "fg", highlight = "Normal" },
+			bg = { attribute = "bg", highlight = "Normal" },
+		},
 
---     tab = {
---       fg = { attribute = "fg", highlight = "TabLine" },
---       bg = { attribute = "bg", highlight = "TabLine" },
---     },
+		tab = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
 
---     tab_close = {
---       -- fg = {attribute='fg',highlight='LspDiagnosticsDefaultError'},
---       fg = { attribute = "fg", highlight = "TabLineSel" },
---       bg = { attribute = "bg", highlight = "Normal" },
---     },
+		tab_close = {
+			-- fg = {attribute='fg',highlight='LspDiagnosticsDefaultError'},
+			fg = { attribute = "fg", highlight = "TabLineSel" },
+			bg = { attribute = "bg", highlight = "Normal" },
+		},
 
---     duplicate_selected = {
---       fg = { attribute = "fg", highlight = "TabLineSel" },
---       bg = { attribute = "bg", highlight = "TabLineSel" },
---       italic = true,
---     },
+		duplicate_selected = {
+			fg = { attribute = "fg", highlight = "TabLineSel" },
+			bg = { attribute = "bg", highlight = "TabLineSel" },
+			italic = true,
+		},
 
---     duplicate_visible = {
---       fg = { attribute = "fg", highlight = "TabLine" },
---       bg = { attribute = "bg", highlight = "TabLine" },
---       italic = true,
---     },
+		duplicate_visible = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+			italic = true,
+		},
 
---     duplicate = {
---       fg = { attribute = "fg", highlight = "TabLine" },
---       bg = { attribute = "bg", highlight = "TabLine" },
---       italic = true,
---     },
+		duplicate = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+			italic = true,
+		},
 
---     modified = {
---       fg = { attribute = "fg", highlight = "TabLine" },
---       bg = { attribute = "bg", highlight = "TabLine" },
---     },
+		modified = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
 
---     modified_selected = {
---       fg = { attribute = "fg", highlight = "Normal" },
---       bg = { attribute = "bg", highlight = "Normal" },
---     },
+		modified_selected = {
+			fg = { attribute = "fg", highlight = "Normal" },
+			bg = { attribute = "bg", highlight = "Normal" },
+		},
 
---     modified_visible = {
---       fg = { attribute = "fg", highlight = "TabLine" },
---       bg = { attribute = "bg", highlight = "TabLine" },
---     },
+		modified_visible = {
+			fg = { attribute = "fg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
 
---     separator = {
---       fg = { attribute = "bg", highlight = "TabLine" },
---       bg = { attribute = "bg", highlight = "TabLine" },
---     },
+		separator = {
+			fg = { attribute = "bg", highlight = "TabLine" },
+			bg = { attribute = "bg", highlight = "TabLine" },
+		},
 
---     separator_selected = {
---       fg = { attribute = "bg", highlight = "Normal" },
---       bg = { attribute = "bg", highlight = "Normal" },
---     },
+		separator_selected = {
+			fg = { attribute = "bg", highlight = "Normal" },
+			bg = { attribute = "bg", highlight = "Normal" },
+		},
 
---     indicator_selected = {
---       fg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" },
---       bg = { attribute = "bg", highlight = "Normal" },
---     },
---   },
--- }
+		indicator_selected = {
+			fg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" },
+			bg = { attribute = "bg", highlight = "Normal" },
+		},
+	}
+end
+
+setup = vim.tbl_extend("keep", { highlights = highlights }, setup)
+bufferline.setup(setup)
