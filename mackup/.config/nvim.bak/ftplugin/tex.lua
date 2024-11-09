033eff02a8fbe94ac6802debe2860be5345afdab
RebornHugo <kiss152535@gmail.com>
2024-11-09 16:05:58 +0800

    backup nvim

---
+++ mackup/.config/nvim.bak/ftplugin/tex.lua
@@ -0,0 +1,71 @@
+if vim.g.isInkscape then
+  return function(_) end
+end
+
+vim.cmd([[
+  setlocal fillchars=fold:\
+  setlocal foldlevel=5
+  setlocal foldmethod=expr
+  setlocal foldexpr=vimtex#fold#level(v:lnum)
+  setlocal foldtext=vimtex#fold#text()
+]])
+
+-- local which_key = require("which-key")
+-- local options = require("config.global").which_key_vars.options
+
+
+-- options = vim.tbl_deep_extend("force", {
+--   filetype = "tex",
+--   buffer = vim.api.nvim_get_current_buf(),
+-- }, options)
+
+-- which_key.register({
+--   ["L"] = {
+--     name = "Language",
+--     m = { "<cmd>VimtexContextMenu<CR>", "Open Context Menu" },
+--     u = { "<cmd>VimtexCountLetters<CR>", "Count Letters" },
+--     w = { "<cmd>VimtexCountWords<CR>", "Count Words" },
+--     d = { "<cmd>VimtexDocPackage<CR>", "Open Doc for package" },
+--     e = { "<cmd>VimtexErrors<CR>", "Look at the errors" },
+--     s = { "<cmd>VimtexStatus<CR>", "Look at the status" },
+--     a = { "<cmd>VimtexToggleMain<CR>", "Toggle Main" },
+--     v = { "<cmd>VimtexView<CR>", "View pdf" },
+--     i = { "<cmd>VimtexInfo<CR>", "Vimtex Info" },
+--     t = { "<cmd>VimtexTocToggle<CR>", "Toggle TOC" },
+--     l = {
+--       name = "Clean",
+--       l = { "<cmd>VimtexClean<CR>", "Clean Project" },
+--       c = { "<cmd>VimtexClean<CR>", "Clean Cache" },
+--     },
+--     c = {
+--       name = "Compile",
+--       c = { "<cmd>VimtexCompile<CR>", "Compile Project" },
+--       o = {
+--         "<cmd>VimtexCompileOutput<CR>",
+--         "Compile Project and Show Output",
+--       },
+--       s = { "<cmd>VimtexCompileSS<CR>", "Compile project super fast" },
+--       e = { "<cmd>VimtexCompileSelected<CR>", "Compile Selected" },
+--     },
+--     r = {
+--       name = "Reload",
+--       r = { "<cmd>VimtexReload<CR>", "Reload" },
+--       s = { "<cmd>VimtexReloadState<CR>", "Reload State" },
+--     },
+--     o = {
+--       name = "Stop",
+--       p = { "<cmd>VimtexStop<CR>", "Stop" },
+--       a = { "<cmd>VimtexStopAll<CR>", "Stop All" },
+--     },
+--   },
+-- }, options)
+
+-- local keymap = require("config.global").keymap
+-- local opts = require("config.global").opts
+-- keymap(
+--   "n",
+--   "<C-f>",
+--   ":silent exec '!inkscape-figures edit \"'.b:vimtex.root.'/figures/\" > /dev/null 2>&1 &'<CR><CR>:redraw!<CR>",
+--   opts,
+--   "Create new figure."
+-- )
