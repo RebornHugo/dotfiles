80ce03b2af1412c0f77b081886f2323ea66ac194
RebornHugo <kiss152535@gmail.com>
2024-11-09 17:30:14 +0800

    config keymap and gitsign

--- mackup/.config/nvim/lua/config/keymaps.lua
+++ mackup/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Shorten function name
+local map = LazyVim.safe_keymap_set
+
+map("n", "<backspace>", "<cmd>nohlsearch<CR>", { desc = "clear highligh" })
+map("n", "<S-q>", function()
+  Snacks.bufdelete()
+end, { desc = "Delete Buffer and Window" })
+
+-- Better paste without refill clipboard by deleted content
+map("v", "p", "P", {})
+map("v", "P", "p", {})
